8a0911fe96604df2bc61bb7ea8b66ca1625bc815
Milli-saved <million12.tenkir@gmail.com>
2023-07-27 14:42:20 +0300

    Build the second challenge.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,111 @@
 import { useState } from "react";
+import "./index.css";
 
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
-
-function App() {
-  const [step, setStep] = useState(1);
-  const handlePrevious = () => {
-    if (step > 1) setStep(step - 1);
-  };
-  const handleNext = () => {
-    if (step < 3) setStep(step + 1);
-  };
+export default function App() {
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="message">
-        Step: {step},{messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
+    <div className="App">
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>Step: {step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+// import { useState } from "react";
+
+// const messages = [
+//   "Learn React ⚛️",
+//   "Apply for jobs 💼",
+//   "Invest your new income 🤑",
+// ];
+
+// function App() {
+//   const [step, setStep] = useState(1);
+//   const handlePrevious = () => {
+//     if (step > 1) setStep(step - 1);
+//   };
+//   const handleNext = () => {
+//     if (step < 3) setStep(step + 1);
+//   };
+//   return (
+//     <div className="steps">
+//       <div className="numbers">
+//         <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+//         <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+//         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+//       </div>
+//       <p className="message">
+//         Step: {step},{messages[step - 1]}
+//       </p>
+//       <div className="buttons">
+//         <button
+//           style={{ backgroundColor: "#7950f2", color: "#fff" }}
+//           onClick={handlePrevious}
+//         >
+//           Previous
+//         </button>
+//         <button
+//           style={{ backgroundColor: "#7950f2", color: "#fff" }}
+//           onClick={handleNext}
+//         >
+//           Next
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
